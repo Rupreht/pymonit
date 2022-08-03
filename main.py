78eb20cef7fc84ac70ba712f7ee2fc7753159da3
Nikolay Ermolovich <rupreht@rupreht.ru>
2022-08-04 00:31:58 +0500

    -

--- main.py
+++ main.py
@@ -15,6 +15,7 @@ networs = summarize_address_range(IPv4Address('192.168.100.0'),
 #                                   IPv4Address('192.168.104.255'))
 loker = RLock()
 system_info_list = []
+status_api_list = []
 
 # url = 'http://192.168.104.154/cgi-bin/get_kernel_log.cgi'
 # url = 'http://192.168.104.154/cgi-bin/minerConfiguration.cgi'
@@ -44,10 +45,33 @@ def get_system_info(hostname: str) -> dict:
     loker.release()
     return obj
 
+def get_status_api(hostname: str) -> dict:
+    """ Get System Info """
+    url = f"http://{hostname}/cgi-bin/get_status_api.cgi"
+    obj = {'hostname': str(hostname)}
+    try:
+        request = requests.get(url,
+                         auth=HTTPDigestAuth(
+                             os.getenv('ASIC_USERNAME'),
+                             os.getenv('ASIC_PASSWD')),
+                         timeout=5)
+        obj['info'] = request.text.strip().split('|')
+    except requests.exceptions.RequestException:
+        obj['info'] = []
+    loker.acquire()
+    status_api_list.append(obj)
+    loker.release()
+    return obj
+
 def get_system_info_in_pool(hostname: str) -> str:
     """ Get Miner Type """
     with pool:
         get_system_info(hostname)
+
+def get_status_api_in_pool(hostname: str) -> str:
+    """ Get Miner Status """
+    with pool:
+        get_status_api(hostname)
 
 def get_addr(iterator_nets, new_prefix=30):
     """
@@ -78,6 +102,19 @@ def discovery_hosts():
     with open("data/discovery_hosts.json", "w", encoding="utf-8") as file:
         json.dump(system_info_list, file, indent=4, ensure_ascii=False)
 
+def get_status_api_all(sys_info_list: list) -> None:
+    """ Get Status API All Hosts """
+    if not sys_info_list:
+        with open("data/discovery_hosts.json", "r", encoding="utf-8") as file:
+            sys_info_list = json.load(file)
+
+    for host in sys_info_list:
+        if host['info']['minertype'] != "n/a":
+            continue
+        print(host['hostname'])
+    return None
+
+
 # def main() -> None:
 #     """ Main """
 #     thr_list = []
@@ -91,8 +128,9 @@ def discovery_hosts():
 #         i.join()
 
 if __name__ == '__main__':
-    pool = BoundedSemaphore(value=25)
-    discovery_hosts()
+    pool = BoundedSemaphore(value=50)
+    # discovery_hosts()
+    get_status_api_all(system_info_list)
     # Check thread's return value
     # main()
     print(f"--- {time.time() - start_time} seconds ---")
